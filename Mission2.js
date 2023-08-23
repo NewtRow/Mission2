93f03fda30b9b100aa008c607bed66a23adfed06
NewtRow <newtonrowe@gmail.com>
2023-08-24 01:34:44 +1200

    i have added some css to my html.

--- Mission2.js
+++ Mission2.js
@@ -37,3 +37,24 @@ if (number1 > number2) {
 }else {
     console.log(`${number1} is not greater than ${number2}`);
 }
+
+// this is the start of my attempt to add in NASA images to my html
+
+async function fetchEarthImage() {
+    try {
+        const response = await fetch('https://api.nasa.gov/EPIC/api/natural/images?api_key=LtKXQLQtnWOLtffuWS5VXAE4RdoBwBKWeSKfTKFb');
+        const data = await response.json();
+
+        if (data && data.length > 0) {
+            const imageUrl = `https://epic.gsfc.nasa.gov/archive/natural/${data[0].date.replace(/-/g, '/')}/png/${data[0].image}.png`;
+            document.body.style.backgroundImage = `url(${imageUrl})`;
+            console.log('Image URL:', imageUrl); // Added in for logging
+        }
+    } catch (error) {
+        console.error('Error fetching Earth image:', error);
+    }
+    
+}
+
+// fetching earth images when page loads
+window.addEventListener('load', fetchEarthImage);
